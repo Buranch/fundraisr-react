f2f6dbb6bbc75a1347706060399ba79a24d98de2
varya <vlada_kuz2011@mail.ru>
2018-02-13 04:13:40 +0500

    update donors

--- src/routes/app/routes/donors/components/donors.js
+++ src/routes/app/routes/donors/components/donors.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import Helper from './helper';
+import DonorTable from './table';
+
+function getMonth(date) {
+  const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  return month[date.getMonth()];
+}
+const date = new Date();
+const dateString = `${getMonth(date)} ${date.getDate()}, ${date.getFullYear()}`;
+
+const HeadText = () => (
+  <div className="row">
+    <div className="col-sm-6">
+      <h1 className="article-title">Donors</h1>
+    </div>
+    <div className="col-sm-6" style={{textAlign: 'right'}}>
+      <h1 className="article-title">{dateString}</h1>
+    </div>
+  </div>
+);
 
 class StartHelper extends React.Component {
-  state = {
-  };
-
-  clickHandle = () => {
-    this.setState({display: 'none'});
-  };
   render() {
     return (
-      <div className="box bg-color-warning" style={this.state} onClick={this.clickHandle}>
-        <div className="box-body">
-          Welcome
-        </div>
-      </div>
+      <section className="container-fluid no-breadcrumbs">
+        <Helper />
+        <HeadText />
+        <DonorTable />
+      </section>
     );
   }
 }
---
+++ src/routes/app/routes/donors/components/table.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {Table} from 'antd';
+import 'antd/dist/antd.css';
+import ControlPanel from './controlPanel';
+import FilterManager from './filterManager';
+
+const dataSource = [{
+  key: '1',
+  firstName: 'Optimus',
+  lastName: 'Prime',
+  email: 'opp@mail.com',
+  companyName: 'Non-profit America',
+  address: '1357 Winners Circle',
+  city: 'Donerville',
+  state: 'CA',
+  zip: '92110'
+}, {
+  key: '2',
+  firstName: 'Daffy',
+  lastName: 'Duck',
+  email: 'dafDuck@mail.com',
+  companyName: 'Ducky Dollars',
+  address: '1 Lilly Lane',
+  city: 'Duckville',
+  state: 'CA',
+  zip: '92345'
+}];
+const col = [{
+  title: 'First Name',
+  dataIndex: 'firstName',
+  key: 'firstName',
+  sorter: (a, b) => a.firstName.localeCompare(b.firstName),
+}, {
+  title: 'Last Name',
+  dataIndex: 'lastName',
+  key: 'lastName',
+  render: text => <a href="#">{text}</a>,
+  sorter: (a, b) => a.lastName.localeCompare(b.lastName),
+}, {
+  title: 'eMail',
+  dataIndex: 'email',
+  key: 'email',
+  sorter: (a, b) => a.email.localeCompare(b.email),
+}, {
+  title: 'Company Name',
+  dataIndex: 'companyName',
+  key: 'companyName',
+  sorter: (a, b) => a.companyName.localeCompare(b.companyName),
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+  sorter: (a, b) => a.address.localeCompare(b.address),
+}, {
+  title: 'City',
+  dataIndex: 'city',
+  key: 'city',
+  sorter: (a, b) => a.city.localeCompare(b.city),
+}, {
+  title: 'State',
+  dataIndex: 'state',
+  key: 'state',
+  sorter: (a, b) => a.state.localeCompare(b.state),
+}, {
+  title: 'ZIP',
+  dataIndex: 'zip',
+  key: 'zip',
+  sorter: (a, b) => a.zip.localeCompare(b.zip),
+}];
+const rowSelection = {};
+
+// TODO add the data
+// TODO add a number of showed rows
+class DonorTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <FilterManager />
+        <div className="box box-default">
+          <div className="box-body">
+            <ControlPanel />
+            <Table
+              dataSource={dataSource}
+              columns={col}
+              size="small"
+              bordered
+              rowSelection={rowSelection}
+            />
+            <ControlPanel />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = DonorTable;
