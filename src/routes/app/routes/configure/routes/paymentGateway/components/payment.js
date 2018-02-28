a5a05a458e0864070a53fa9ef9495abccfcb0e0d
varya <vlada_kuz2011@mail.ru>
2018-02-28 05:27:05 +0500

    finish payment gateway

--- src/routes/app/routes/configure/routes/paymentGateway/components/payment.js
+++ src/routes/app/routes/configure/routes/paymentGateway/components/payment.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import Helper from './helper';
+import Helper from '../../../../../components/helper';
+import HeadText from '../../../../../components/headText';
+import Breadcrumb from '../../../../../components/breadcrumb';
 import PayPal from './paypal';
 import CreditCards from './creditCards';
 import BlockCountries from './blockCountries';
 import BlockIP from './blockIP';
 
-function getMonth(date) {
-  const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-  return month[date.getMonth()];
-}
-const date = new Date();
-const dateString = `${getMonth(date)} ${date.getDate()}, ${date.getFullYear()}`;
-
-const HeadText = () => (
-  <div className="row">
-    <div className="col-sm-6">
-      <h1 className="article-title">Organisation information</h1>
-    </div>
-    <div className="col-sm-6" style={{textAlign: 'right'}}>
-      <h1 className="article-title">{dateString}</h1>
-    </div>
-  </div>
-);
-
 const Main = () => (
   <div className="row">
-    <div className="col-xl-6">
+    <div className="col-xs-12 col-sm-12 col-md-6">
       <div className="box box-default">
         <div className="box-body">
           <h5>Donor Payment Options</h5>
@@ -36,7 +20,7 @@ const Main = () => (
         </div>
       </div>
     </div>
-    <div className="col-xl-6">
+    <div className="col-xs-12 col-sm-12 col-md-6">
       <div className="box box-default">
         <div className="box-body">
           <h5>Donor Payment Options</h5>
@@ -54,7 +38,12 @@ const Main = () => (
 const paymentGateway = () => (
   <section className="container-fluid no-breadcrumbs">
     <Helper />
-    <HeadText />
+    <HeadText pageName="Payment Gateway" />
+    <Breadcrumb path={
+      <small>
+        <a href="#app/dashboard">Dashboard</a> >> <a>Configure</a> >> Payment Gateway
+      </small>
+    } />
     <Main />
   </section>
 );
