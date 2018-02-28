68bc3908c21ea5f27546cde17612ea4c7e31e862
varya <vlada_kuz2011@mail.ru>
2018-02-28 05:59:11 +0500

    finish api

--- src/routes/app/routes/configure/routes/apiAccess/components/apiAccess.js
+++ src/routes/app/routes/configure/routes/apiAccess/components/apiAccess.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import Helper from './helper';
+import Helper from '../../../../../components/helper';
+import HeadText from '../../../../../components/headText';
+import Breadcrumb from '../../../../../components/breadcrumb';
 import Keys from './keys';
-
-function getMonth(date) {
-  const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-  return month[date.getMonth()];
-}
-const date = new Date();
-const dateString = `${getMonth(date)} ${date.getDate()}, ${date.getFullYear()}`;
-
-const HeadText = () => (
-  <div className="row">
-    <div className="col-sm-6">
-      <h1 className="article-title">Organisation information</h1>
-    </div>
-    <div className="col-sm-6" style={{textAlign: 'right'}}>
-      <h1 className="article-title">{dateString}</h1>
-    </div>
-  </div>
-);
 
 const apiAccess = () => (
   <section className="container-fluid no-breadcrumbs">
     <Helper />
-    <HeadText />
+    <HeadText pageName="Api Access" />
+    <Breadcrumb path={
+      <small>
+        <a href="#app/dashboard">Dashboard</a> >> <a>Configure</a> >> Api Access
+      </small>
+    } />
     <Keys />
   </section>
 );
